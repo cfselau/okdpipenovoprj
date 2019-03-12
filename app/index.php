ddbe5ed67e425ff1a63c7ec40014d72ab56c6877
cfselau <35224690+cfselau@users.noreply.github.com>
2019-03-12 16:04:37 -0300

    Add files via upload

--- app/index.php
+++ app/index.php
@@ -1,1 +1,16 @@
-sss
+<html>
+  <head>
+    <title>Hello OKD</title>
+  </head>
+  <body bgcolor="#FFF333">
+    <h1>
+<?php
+
+$name = $_GET['name'];
+
+echo "Hello $name";
+
+?>
+    </h1>
+  </body>
+</html>
